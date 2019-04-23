0f576256d74a7ef79b3e88e5f9855e47e0e00f38
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2019-04-23 14:48:00 -0700

    Add pointer functionality

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /*
     Given a character pointer x (that points to an array of chars), and a
@@ -15,7 +16,14 @@
 */
 void string_copy(char *x, char *y)
 {
+	int i = 0;
 
+	while (*(y + i) != '\0')
+	{
+		*(x + i) = *(y + i);
+		i++;
+	}
+	*(x + i) = '\0';
 }
 
 /*
@@ -28,7 +36,23 @@ void string_copy(char *x, char *y)
 */
 char *find_char(char *str, int c)
 {
+	int i = 0;
+	char *p;
 
+	while (*(str + i) != '\0')
+	{
+		p = (str + i);
+		if (*(str + i) == c)
+		{
+			break;
+		}
+		i++;
+	}
+	if (*(p + 1) == '\0')
+	{
+		return NULL;
+	}
+	return p;
 }
 
 /*
@@ -41,18 +65,54 @@ char *find_char(char *str, int c)
 */
 char *find_string(char *haystack, char *needle)
 {
+	int haystack_length = strlen(haystack);
+	int needle_length = strlen(needle);
+	int found = 0;
+	char *p = NULL;
 
+	if (haystack_length >= needle_length)
+	{
+		for (int i = 0, t = 0; i < haystack_length; i++)
+		{
+			do
+			{
+				if (haystack[i] == needle[t])
+				{
+					if (p == NULL)
+					{
+						p = &haystack[i];
+					}
+					if (++found == needle_length)
+					{
+						return p;
+					}
+					i++;
+					t++;
+				}
+				else
+				{
+					i -= found;
+					found = 0;
+					t = 0;
+					p = NULL;
+				}
+			} while (found);
+		}
+		return NULL;
+	}
+	else
+		return NULL;
 }
 
 #ifndef TESTING
 int main(void)
 {
-    char *found_char = find_char(hello, 'e');
-    char *found_string = find_string(world, "or");
+	char *found_char = find_char(hello, 'e');
+	char *found_string = find_string(world, "or");
 
-    printf("Found char: %s\n", found_char);
-    printf("Found string: %s\n", found_string);
+	printf("Found char: %s\n", found_char);
+	printf("Found string: %s\n", found_string);
 
-    return 0;
+	return 0;
 }
 #endif
